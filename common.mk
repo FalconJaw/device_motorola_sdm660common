7649b650ee777900823cf5f23e0fae44c3c82ea9
FalconJaw <gaituoshinra@gmail.com>
2021-05-15 04:06:10 +0000

    Remove LineageOS HALs

--- common.mk
+++ common.mk
@@ -206,9 +206,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sdm660
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.0-service-sdm \
+#    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -370,8 +370,8 @@ PRODUCT_PACKAGES += \
     libtinyxml2
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
